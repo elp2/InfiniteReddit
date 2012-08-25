605f2e605d0ea1ddbb0f3cb30923bd691a3566bb
Edward Palmer <edward.palmer@gmail.com>
2012-08-25 19:06:53 +0200

    over_18 skipping

--- public/reddit.js
+++ public/reddit.js
@@ -20,6 +20,8 @@ function urlForSubreddits(subreddits, after ) {
 }
 
 function getImage(item) {
+	if(!shouldShowImage(item))
+		return undefined
 
 	if( !item.url.endsWith("png") && !item.url.endsWith("jpg") && !item.url.endsWith("jpeg") )
 		return undefined
@@ -31,6 +33,23 @@ redditAfterTag = ""
 
 seenURLs = {}
 cbURLs = {}
+
+showOver18 = false;
+numOver18Skipped = 0;
+function skippedOver18(item) {
+	console.log("skipping over_18")
+	numOver18Skipped++;
+}
+
+function shouldShowImage(item) {
+	if(undefined == item)
+		return false
+	if(true == item.over_18 && !showOver18) {
+		skippedOver18(item);
+		return false;
+	}
+
+}
 
 function createImage( listView, img ) {
 
